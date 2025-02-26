ada7bca19a1d1893017b53477aad77b09b21b07b
kh4idvng <dang.nk.64cntt@ntu.edu.vn>
2025-02-26 18:07:48 +0700

    Hoàn thiện code xử lý

--- Ex3_SimpleSumApp/app/src/main/java/ntu/ngokhaidang/ex3_simplesumapp/MainActivity.java
+++ Ex3_SimpleSumApp/app/src/main/java/ntu/ngokhaidang/ex3_simplesumapp/MainActivity.java
@@ -1,6 +1,8 @@
 package ntu.ngokhaidang.ex3_simplesumapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,32 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    //bo lang nghe va xu ly su kien len nut tinh tong
+    public void XuLyCong(View view){
+        //Tim , tham chieu bien den dieu khien tren tep XML
+        EditText editTextSoA = findViewById(R.id.edtA);
+        EditText editTextSoB = findViewById(R.id.edtB);
+        EditText editTextKetQua = findViewById(R.id.edtKQ);
+        //lay du lieu ve o dieu khien A
+        String strA = editTextSoA.getText().toString();
+        //lay du lieu ve o dieu khien B
+        String strB = editTextSoB.getText().toString();
+
+        //chuyen du lieu sang dang so
+        int so_A = Integer.parseInt(strA);
+        int so_B = Integer.parseInt(strB);
+
+        //Tinh toan theo yeu cau
+        int tong = so_A + so_B;
+        String strTong = String.valueOf(tong);
+
+        //Hien ra man hinh
+        editTextKetQua.setText(strTong);
+
+
+
+
     }
 }
