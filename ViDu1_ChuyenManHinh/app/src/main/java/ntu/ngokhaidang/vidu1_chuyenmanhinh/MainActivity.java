5064c1f9508fefcfe17d18fcf0922b2ebbfeeb69
kh4idvng <dang.nk.64cntt@ntu.edu.vn>
2025-03-07 10:20:35 +0700

    hoàn thành code chuyển màn hình ( đã hoạt động )

--- ViDu1_ChuyenManHinh/app/src/main/java/ntu/ngokhaidang/vidu1_chuyenmanhinh/MainActivity.java
+++ ViDu1_ChuyenManHinh/app/src/main/java/ntu/ngokhaidang/vidu1_chuyenmanhinh/MainActivity.java
@@ -1,6 +1,8 @@
 package ntu.ngokhaidang.vidu1_chuyenmanhinh;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,5 +23,13 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
     }
-    //
+    //Ham dap ung su kien nhan len nut "Sang man hinh khac"
+    //Xu ly chuyen man hinh
+    public void ChuyenManHinh(View view){
+        //Tao mot doi tuong Intent
+        //Tham so thu 2 cua ham tao nay, la ten Activity (man hinh) ta muon chuyen sang
+        Intent iManHinhKhac = new Intent(this, SubActivityOne.class);
+        //thuc hien chuyen
+        startActivity(iManHinhKhac);
+    }
 }
--- ViDu1_ChuyenManHinh/app/src/main/java/ntu/ngokhaidang/vidu1_chuyenmanhinh/SubActivityOne.java
+++ ViDu1_ChuyenManHinh/app/src/main/java/ntu/ngokhaidang/vidu1_chuyenmanhinh/SubActivityOne.java
@@ -1,6 +1,8 @@
 package ntu.ngokhaidang.vidu1_chuyenmanhinh;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +10,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class activity_sub_one extends AppCompatActivity {
+public class SubActivityOne extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +22,14 @@ public class activity_sub_one extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    //Ham dap ung su kien nhan len nut "Quay ve trang chu"
+    //Xu ly chuyen ve man hinh trang chu
+    public void ChuyenManHinh(View view){
+        //Tao mot doi tuong Intent
+        //Tham so thu 2 cua ham tao nay, la ten Activity (man hinh) ta muon chuyen sang
+        Intent iManHinhCHINH = new Intent(this, MainActivity.class);
+        //thuc hien chuyen
+        startActivity(iManHinhCHINH);
     }
 }
