8b2214a7c375a3ff0456eb04e20c8945acdad670
kh4idvng <dang.nk.64cntt@ntu.edu.vn>
2025-03-10 10:16:42 +0700

    da tao xong giao dien, code chua xong

---
+++ Ex6_IntentDonGian/app/src/main/java/ntu/ngokhaidang/ex6_intentdongian/MainActivity.java
@@ -0,0 +1,35 @@
+package ntu.ngokhaidang.ex6_intentdongian;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    Button nutMH2;
+    Button nutMH3;
+    void TimDieuKhien(){
+        nutMH2 = (Button) findViewById(R.id.btnMH2);
+        nutMH3 = (Button) findViewById(R.id.btnMH3);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        //Tim dieu khien nut bam
+        TimDieuKhien();
+        //Gan bo lang nghe
+    }
+
+}
