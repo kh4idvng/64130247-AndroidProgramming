e320b58cf68204aa12df26cb5cd35d2dac8f2521
kh4idvng <dang.nk.64cntt@ntu.edu.vn>
2025-03-10 20:07:05 +0700

    code xu ly su kien o man hinh chinh

--- Ex7_IntentLogin/app/src/main/java/ntu/ngokhaidang/ex7_intentlogin/MainActivity.java
+++ Ex7_IntentLogin/app/src/main/java/ntu/ngokhaidang/ex7_intentlogin/MainActivity.java
@@ -1,6 +1,9 @@
 package ntu.ngokhaidang.ex7_intentlogin;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +22,17 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        //tim dk
+        Button btnDangNhap = (Button) findViewById(R.id.btnLogin);
+        //su kien
+        btnDangNhap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //lenh xu ly o day
+                Intent iLogin = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(iLogin);
+            }
         });
     }
 }
