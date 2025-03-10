61572900f2557e18f6c67f85502b1b114fb1a6b5
kh4idvng <dang.nk.64cntt@ntu.edu.vn>
2025-03-10 20:17:54 +0700

    code xu ly man hinh dang nhap

--- Ex7_IntentLogin/app/src/main/java/ntu/ngokhaidang/ex7_intentlogin/LoginActivity.java
+++ Ex7_IntentLogin/app/src/main/java/ntu/ngokhaidang/ex7_intentlogin/LoginActivity.java
@@ -1,6 +1,11 @@
 package ntu.ngokhaidang.ex7_intentlogin;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +24,40 @@ public class LoginActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button btnXacNhan = (Button) findViewById(R.id.btnOK);
+
+        btnXacNhan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // xu ly dang nhap
+                // lay du lieu
+                //B1. tim tham chieu dk
+                EditText edTenDN = (EditText) findViewById(R.id.edtUsername);
+                EditText edPass = (EditText) findViewById(R.id.edtPassword);
+                //B2. lay du lieu
+                String tenDangNhap = edTenDN.getText().toString();
+                String mk = edPass.getText().toString();
+                //Kiem tra mat khau
+                if (tenDangNhap.equals("ngokhaidang") && mk.equals("123")) // mk dung
+                {
+                    //chuyen sang man hinh home
+                    Intent iQuiz = new Intent(LoginActivity.this, HomeActivity.class);
+                    //Goi du lieu vao iQuiz, dang key - value; key duoc dung de ben kia loc ra du lieu
+                    iQuiz.putExtra("ten_dang_nhap", tenDangNhap);
+                    iQuiz.putExtra("mk_dang_nhap", mk);
+                    //gui di
+                    startActivity(iQuiz);
+
+                }
+                else
+                {
+                    Toast.makeText(LoginActivity.this, "BẠN NHẬP SAI THÔNG TIN",Toast.LENGTH_LONG);
+
+                }
+
+            }
         });
     }
 }
