5fb32f912f5141bdf45a10e155224ed3e0c1f268
kh4idvng <dang.nk.64cntt@ntu.edu.vn>
2025-03-12 17:47:35 +0700

    hoan thanh code xu ly

--- LVNgonNguLapTrinh/app/src/main/java/ntu/ngokhaidang/lvngonngulaptrinh/MainActivity.java
+++ LVNgonNguLapTrinh/app/src/main/java/ntu/ngokhaidang/lvngonngulaptrinh/MainActivity.java
@@ -1,6 +1,12 @@
 package ntu.ngokhaidang.lvngonngulaptrinh;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +14,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    ListView listViewNNTL;
+    ArrayList<String> dsNgonNguLT;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +30,30 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        listViewNNTL = findViewById(R.id.lvNNLT);
+
+        //B1. Chuan bi du lieu, hard-code
+        dsNgonNguLT = new ArrayList<String>();
+        dsNgonNguLT.add("Python");
+        dsNgonNguLT.add("php");
+        dsNgonNguLT.add("Java");
+
+        //B2.
+        ArrayAdapter<String> adapterNNLT;
+        adapterNNLT = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, dsNgonNguLT);
+        //B3. Gan adapter
+        listViewNNTL.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //code duoc xu ly truc tiep o day
+                //
+                String giaTriDuocChon = dsNgonNguLT.get(position);
+                //
+                //
+                Toast.makeText(MainActivity.this, giaTriDuocChon,Toast.LENGTH_LONG).show();
+            }
+        });
+
+        
     }
 }
