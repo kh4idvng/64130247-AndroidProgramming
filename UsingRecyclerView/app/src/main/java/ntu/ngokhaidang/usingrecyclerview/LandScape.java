39b69e13bbca31f613dfa49b399d1f6da84dc193
kh4idvng <dang.nk.64cntt@ntu.edu.vn>
2025-03-26 09:18:33 +0700

    generate getter setter va constructor

--- UsingRecyclerView/app/src/main/java/ntu/ngokhaidang/usingrecyclerview/LandScape.java
+++ UsingRecyclerView/app/src/main/java/ntu/ngokhaidang/usingrecyclerview/LandScape.java
@@ -1,4 +1,27 @@
 package ntu.ngokhaidang.usingrecyclerview;
 
 public class LandScape {
+    String landImageFileName;
+    String landCation;
+
+    public LandScape(String landImageFileName, String landCation) {
+        this.landImageFileName = landImageFileName;
+        this.landCation = landCation;
+    }
+
+    public String getLandImageFileName() {
+        return landImageFileName;
+    }
+
+    public void setLandImageFileName(String landImageFileName) {
+        this.landImageFileName = landImageFileName;
+    }
+
+    public String getLandCation() {
+        return landCation;
+    }
+
+    public void setLandCation(String landCation) {
+        this.landCation = landCation;
+    }
 }
