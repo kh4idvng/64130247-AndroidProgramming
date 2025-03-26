2352031bd11c5149aa72b6c3293cdc0054511ccb
kh4idvng <dang.nk.64cntt@ntu.edu.vn>
2025-03-26 10:10:59 +0700

    hoàn thành

--- UsingRecyclerView/app/src/main/java/ntu/ngokhaidang/usingrecyclerview/MainActivity.java
+++ UsingRecyclerView/app/src/main/java/ntu/ngokhaidang/usingrecyclerview/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
@@ -22,6 +23,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        //3
+        recyclerViewDatas = getDataForRecyclerView();
+        //4
+        recyclerViewLandScape = findViewById(R.id.recyclerLand);
+        //5
+        RecyclerView.LayoutManager layoutLinear = new LinearLayoutManager(this);
+        recyclerViewLandScape.setLayoutManager(layoutLinear);
+        //6
+        landScapeAdapter = new LandScapeAdapter(this, recyclerViewDatas);
+        //7
+        recyclerViewLandScape.setAdapter(landScapeAdapter);
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
@@ -31,6 +43,11 @@ public class MainActivity extends AppCompatActivity {
     }
     ArrayList<LandScape> getDataForRecyclerView(){
         ArrayList<LandScape> dsDuLieu = new ArrayList<LandScape>();
-        LandScape landScape1 = new LandScape()
+        LandScape landScape1 = new LandScape("ho", "Con Hổ");
+        dsDuLieu.add(landScape1);
+        dsDuLieu.add(new LandScape("pool", "Ho Boi"));
+        dsDuLieu.add(new LandScape("pool", "Ho Boi"));
+        dsDuLieu.add(new LandScape("cowboy", "Cao Boi Mien Tay Nuoc My"));
+        return dsDuLieu;
     }
 }
