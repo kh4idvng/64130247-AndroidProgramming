e7d18abe3d91c3e379e5074490e4a3bd77258898
kh4idvng <dang.nk.64cntt@ntu.edu.vn>
2025-04-04 15:45:49 +0700

    giao diện màn hình 2 và code xử lý chuyể màn hình

--- thigk2.ngokhaidang/app/src/main/java/thigk2/ngokhaidang/ChucNang2.java
+++ thigk2.ngokhaidang/app/src/main/java/thigk2/ngokhaidang/ChucNang2.java
@@ -1,6 +1,10 @@
 package thigk2.ngokhaidang;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +13,43 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class ChucNang2 extends AppCompatActivity {
+    EditText editTextThang;
+    EditText editTextNam;
+    Button nutKT;
+    void TimDieuKhien(){
+        editTextThang = findViewById(R.id.edtThang);
+        editTextNam = findViewById(R.id.edtNam);
+        nutKT = findViewById(R.id.btnCheck);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_chuc_nang2);
+        TimDieuKhien();
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        nutKT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                KiemTra();
+            }
+        });
+    }
+    public void KiemTra(){
+        String thang = editTextThang.getText().toString();
+        String nam = editTextNam.getText().toString();
+
+        int Thang = Integer.parseInt(thang);
+        int Nam = Integer.parseInt(nam);
+        if (( Thang== 4) && ( Nam == 1975)){
+            Toast.makeText(ChucNang2.this, "Đúng!", Toast.LENGTH_SHORT).show();
+        }else {
+            Toast.makeText(ChucNang2.this, "Sai!", Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- thigk2.ngokhaidang/app/src/main/java/thigk2/ngokhaidang/MainActivity.java
+++ thigk2.ngokhaidang/app/src/main/java/thigk2/ngokhaidang/MainActivity.java
@@ -1,6 +1,9 @@
 package thigk2.ngokhaidang;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +12,34 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    Button nut2;
+    Button nut3;
+    Button nut4;
+    Button nutAM;
+    void TimDieuKhien(){
+        Button nut2 = findViewById(R.id.btnCN2);
+        Button nut3 = findViewById(R.id.btnCN3);
+        Button nut4 = findViewById(R.id.btnCN4);
+        Button nutAM = findViewById(R.id.btnAM);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        TimDieuKhien();
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        nut2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent cn2 = new Intent(MainActivity.this, ChucNang2.class);
+                startActivity(cn2);
+            }
         });
     }
 }
