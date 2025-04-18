b4a86056e8c8d7f60e9474db5cd4a32fe6c63800
kh4idvng <dang.nk.64cntt@ntu.edu.vn>
2025-04-18 20:23:47 +0700

    hoan thanh

--- FragmentExAddDynamic/app/src/main/java/ntu/ngokhaidang/fragmentexadddynamic/MainActivity.java
+++ FragmentExAddDynamic/app/src/main/java/ntu/ngokhaidang/fragmentexadddynamic/MainActivity.java
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +17,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                        .add(R.id.frameContent, new ContentFragment() )
+                        .add(R.id.frameFooter, new FooterFragment())
+                        .commit();
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
